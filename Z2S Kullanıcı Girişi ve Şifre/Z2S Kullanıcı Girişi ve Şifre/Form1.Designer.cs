6f8bea593956d24134144d7b3de29935fb035890
zirem11 <89360252+zirem11@users.noreply.github.com>
2022-12-11 00:30:32 +0300

    deneme 2

--- Z2S Kullanıcı Girişi ve Şifre/Z2S Kullanıcı Girişi ve Şifre/Form1.Designer.cs
+++ Z2S Kullanıcı Girişi ve Şifre/Z2S Kullanıcı Girişi ve Şifre/Form1.Designer.cs
@@ -44,9 +44,9 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(87, 196);
+            this.label1.Location = new System.Drawing.Point(99, 261);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 28);
+            this.label1.Size = new System.Drawing.Size(150, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Adı:";
             // 
@@ -54,9 +54,9 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(154, 245);
+            this.label2.Location = new System.Drawing.Point(176, 327);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 28);
+            this.label2.Size = new System.Drawing.Size(67, 35);
             this.label2.TabIndex = 1;
             this.label2.Text = "Şifre:";
             // 
@@ -64,26 +64,28 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox1.Location = new System.Drawing.Point(227, 195);
+            this.textBox1.Location = new System.Drawing.Point(259, 260);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 29);
+            this.textBox1.Size = new System.Drawing.Size(193, 34);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox2.Location = new System.Drawing.Point(227, 244);
+            this.textBox2.Location = new System.Drawing.Point(259, 325);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 29);
+            this.textBox2.Size = new System.Drawing.Size(193, 34);
             this.textBox2.TabIndex = 3;
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(302, 285);
+            this.linkLabel1.Location = new System.Drawing.Point(345, 380);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(94, 15);
+            this.linkLabel1.Size = new System.Drawing.Size(115, 20);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Şifremi unuttum";
@@ -92,9 +94,10 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(108, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(123, 39);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(288, 160);
+            this.pictureBox1.Size = new System.Drawing.Size(329, 213);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -103,9 +106,10 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // 
             this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.button1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(173, 329);
+            this.button1.Location = new System.Drawing.Point(198, 439);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 34);
+            this.button1.Size = new System.Drawing.Size(174, 45);
             this.button1.TabIndex = 6;
             this.button1.Text = "Giriş";
             this.button1.UseVisualStyleBackColor = false;
@@ -113,10 +117,10 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             // 
             // Kullanici_Girisi_ve_Sifre
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(531, 395);
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(607, 527);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.linkLabel1);
@@ -124,6 +128,7 @@ namespace Z2S_Kullanıcı_Girişi_ve_Şifre
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Kullanici_Girisi_ve_Sifre";
             this.Text = "Kullanici_Girisi_ve_Sifre";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
